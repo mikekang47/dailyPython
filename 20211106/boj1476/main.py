5c0a421aa2e9ab5291d29fa4fd30adcde816d6e6
mikekang47 <gyeong0308@gmail.com>
2021-11-07 23:21:31 +0900

    solve: boj1476

--- 20211106/boj1476/main.py
+++ 20211106/boj1476/main.py
@@ -1,3 +1,20 @@
 import sys
-e, s, m = map(int, sys.stdin.readline().split())
 
+e, m, s = map(int, sys.stdin.readline().split())
+year = 0
+while 1:
+    year += 1
+    E = year % 15
+    M = year % 28
+    S = year % 19
+    if E == 0:
+        E = 15
+    if M == 0:
+        M = 28
+    if S == 0:
+        S = 19
+
+    if E == e and S == s and M == m:
+        break
+
+print(year)
